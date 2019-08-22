1bf67828c15132566015dd491a9ed68f0c9243f6
jlli6 <jie.li6@uq.net.au>
2019-08-22 15:45:11 +1000

    commit wrappters for fastqc and trimmomatic

---
+++ filterfq/trimmomatic.py
@@ -0,0 +1,11 @@
+###################################
+#
+# trimmomatic.py - runs trimmomatic and provides stat result output
+#
+########################################################################
+
+import os
+import sys
+import
+
+
