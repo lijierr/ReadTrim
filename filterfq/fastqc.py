50ef004fe219ebb0de4cd177ab87f1ad45ce1bdf
jlli6 <jie.li6@uq.net.au>
2019-08-23 15:54:51 +1000

    update fastqc.py

--- filterfq/fastqc.py
+++ filterfq/fastqc.py
@@ -9,10 +9,16 @@ import sys
 import subprocess
 import logging
 
+from universal import checkPathExists, makeSurePathExists,checkFilesExist
+
 class fastqcPileup:
 	""" Wrapper for running fastqc """
-	def __init__(self, outDir):
+	def __init__(self):
 		self.logger = logging.getLogger('timestamp')
+		
+		self.checkFastQC()
+
+		
 	def checkFastQC(self):
 		""" Check if FastQC is in system path! """
 		
@@ -23,5 +29,16 @@ class fastqcPileup:
 		except:
 			self.logger.error(" [Error] Make sure FastQC is in system path.")
 			sys.exit(1)
+
+
+	def run(self, fqs, adapters, outDir):
+		
+		checkFilesExist(fqs, adapters)
+
+		makeSurePathExists(outDir)
+
+		os.system("fastqc -a %s -o %s %s 2>/dev/null" % (adapters, outDir, " ".join()))
+		
+		
 		
 
