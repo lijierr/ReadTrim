f2c639cfb8e6172ddcb07c8e84de9bff86955e40
jlli6t <mm.jlli6t@gmail.com>
2020-08-19 17:34:59 +1000

    update version and manifest

--- readtrim/version.py
+++ readtrim/version.py
@@ -1,32 +1,34 @@
 """
-The :mod:`readtrim.version` printing version information.
+The :mod:`readtrim.version` print and show version information.
 """
 
 # Author: Jie Li <mm.jlli6t@gmail.com>
 # License: GNU v3.0
-# Copyrigth: 2020
+# Copyrigth: 2019
 
-import os
+from os.path import join, isfile, split, dirname
 from biosut.gt_path import abs_path
 
 class Version:
+	f_dir = dirname(abs_path(__file__))
+	@classmethod
+	def get_version(cls):
+		ver1 = join(cls.f_dir, 'version')
+		ver2 = join(cls.f_dir + '../version')
+		if isfile(ver1):return open(ver1).readline().strip()
+		if isfile(ver2):return open(ver2).readline().strip()
+		return 'Unknown verion'
 
-    def get_version():
-        ver1 = os.path.dirname(abs_path(__file__)) + '/version'
-        ver2 = os.path.dirname(abs_path(__file__)) + '/../version'
-        if os.path.isfile(ver1):return open(ver1).readline().strip()
-        if os.path.isfile(ver2):return open(ver2).readline().strip()
-        return 'Unknow version'
+	@classmethod
+	def show_version(cls):
+		ver = cls.get_version()
+		name = split(dirname(__file__))[1]
+		print('\n\n\t\t%s version * %s *\n\n' % (name, ver))
 
-    @classmethod
-    def show_version(cls):
-        ver = cls.get_version()
-        name = os.path.split(os.path.dirname(__file__))[1]
-        print('\n\n\t\t%s version * %s *\n\n' % (name, ver))
-
-    def long_description():
-		readme1 = join(f_dir, 'README.md')
-		readme2 = join(f_dir, '/../README.md')
+	@classmethod
+	def long_description(cls):
+		readme1 = join(cls.f_dir, 'README.md')
+		readme2 = join(cls.f_dir, '/../README.md')
 		if isfile(readme1):return open(readme1).read()
 		if isfile(readme2):return open(readme2).read()
 		return "No detailed description"
