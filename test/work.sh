c3fd9ce67268ee7003345a97f4430dfb79705600
jlli6t <jlli6t@gmail.com>
2021-09-28 11:01:02 +1000

    module for read trimming

--- test/work.sh
+++
@@ -1,4 +0,0 @@
-#python3 /home/s4506266/bin/0.Metagenomics/Filter_module/Filter_Trimmomatic/bin/Filter_trimmomatic-1.2.py --rawData_list fq.list --read_length 300 --minlength 220 --phred 33 --adapters adapters.txt --sliding_window 6:20 --outdir /90days/s4506266/2.huayu_11_samples_16S/1.filter/6-20
-
-/30days/s4506266/softwares/anaconda3/bin/python /home/s4506266/bin/4.bin/Filter_Trimmomatic/bin/Filter_trimmomatic-1.2.py --rawData_list fq_DNA.list --read_length 151 --minlength 75 --phred 33 --adapters adapters.txt --sliding_window 6:20 --outdir /90days/s4506266/5.liutao_6samples/1.4samples_metagenome/1.Filter
-echo done
