21d7fbe321bb0992307df9b4a551f91d36ffdb48
jlli6t <jlli6t@gmail.com>
2023-03-01 21:52:06 +1000

    remove work.sh

--- work.sh
+++
@@ -1,2 +0,0 @@
-/30days/s4506266/softwares/anaconda3/bin/python /home/s4506266/bin/0.Metagenomics/FastQC/bin/fastQC-v1.0.py --fqlist fq.list --outdir /home/s4506266/bin/0.Metagenomics/FastQC/test
-echo done
